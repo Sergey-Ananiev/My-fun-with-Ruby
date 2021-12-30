97581f670c1fa4398fb23a62358cdb154d5d8eb7
Сергей Ананьев <cosmer16@gmail.com>
2021-12-30 19:44:26 +0200

    Ruby commit

---
+++ WekndPrgr.rb
@@ -0,0 +1,12 @@
+time = Time.now 
+week_day = time.wday 
+
+if (week_day == 0 || week_day == 6)
+	puts ("Сегодня выходной!!!")
+else
+	puts ("Дуй на работу!!!")
+end
+
+
+
+		
---
+++ orel-reshka.rb
@@ -0,0 +1,8 @@
+if rand(2) == 1
+  puts "Выпала решка"
+elsif rand(2) == 0
+  puts "Выпал орел"
+elsif rand(11) == 10 
+  puts "Ребро!!!"
+end
+
